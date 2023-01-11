3f173b64210d9c4f45761de38e10e156f116fd3a
yexi <2794957189@qq.com>
2023-01-11 21:24:23 +0800

    起始

---
+++ src/pages/user/IndexBase.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="base">
+
+  </div>
+</template>
+
+<script>
+export default {
+name:'IndexBase'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/user/zffUserRouter.js
+++ src/router/user/zffUserRouter.js
@@ -1,4 +1,13 @@
 // 引入路由
-
+import IndexBase from '@/pages/user/IndexBase'
 // 
-export default []
+export default [
+    {
+        path:'/IndexBase',
+        name:'IndexBase',
+        component:IndexBase,
+        meta:{
+        title:"课程空间"
+        },
+    },
+]
