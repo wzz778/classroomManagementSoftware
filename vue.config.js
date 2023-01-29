cadd941a60d90296ee56bbbc6adcd94764ae211d
yexi <2794957189@qq.com>
2023-01-29 19:19:47 +0800

    大致直播弹幕

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+  },
   transpileDependencies: true,
   devServer: {
     proxy: {
