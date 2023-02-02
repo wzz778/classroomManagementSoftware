a6f9c5c8b69e211b8e58dc81289fab9e0ff6371c
yexi <2794957189@qq.com>
2023-02-02 08:53:34 +0800

    教师信息

--- src/pages/student/IndexCourse.vue
+++ src/pages/student/IndexCourse.vue
@@ -14,24 +14,15 @@
             <div class="teacher">
               <div class="course_name">{{ lesson.name }}</div>
               <p style="text-align: left">
-                主讲人:<span>叶子</span>教室团队：<span>一人</span>
+                主讲人:<span>叶子</span>教室团队：<span>1人</span>
               </p>
             </div>
             <div class="infor">
               <ul>
-                <li>学校：河南科技学院</li>
-                <li>学分：2分</li>
-                <li>开课院系：文学学院</li>
-                <li>课时：64个</li>
-                <li>行政班级：计科213</li>
-                <li>编号：1111</li>
+                <li>课程码：{{ lesson.cord }}</li>
+                <li>创建时间：{{ lesson.beginTime }}</li>
               </ul>
-              <myLive/>
-              <el-button type="primary" round
-                ><router-link to="/user/IndexBase"
-                  >进入课程</router-link
-                ></el-button
-              >
+              <myLive />
             </div>
           </div>
         </div>
@@ -46,11 +37,28 @@
             </div>
           </el-tab-pane>
           <el-tab-pane label="老师介绍" name="second">
-            <div class="les">
-              同学们好
-              <p>
-                金秋校园，书韵馨香！《论语》是儒家的一部核心经典，千百年来是莘莘学子必读之书目，世人修身之法则。宋代丞相赵普盛赞道：“半部《论语》治天下”。可见，《论语》是和谐人伦关系的金科玉律，是修齐治平，协和万邦的智慧宝典，让我们共读《论语》，传承圣贤文化，开启福慧人生！
-              </p>
+            <div class="tes">
+              <div class="tesmain">
+                <div>
+                  <img
+                    :src="
+                      teacher.cover ? teacher.cover : require('@/assets/01.jpg')
+                    "
+                  />
+                </div>
+                <div>编号：{{teacher.tcode}}</div>
+                <div>姓名：{{teacher.tname}}</div>
+                <div>邮箱：{{teacher.email}}</div>
+              </div>
+              <div class="tesmain">
+                <div>
+                  性别： <el-radio disabled v-model="teacher.sex" label="男">男</el-radio>
+                  <el-radio disabled v-model="teacher.sex" label="女" style="margin-left: 20px"
+                    >女</el-radio
+                  >
+                </div>
+                <div>地址：{{teacher.place}}</div>
+              </div>
             </div>
           </el-tab-pane>
         </el-tabs>
@@ -61,8 +69,14 @@
 
 <script>
 import myLive from "@/components/teacher/utilComponents/myLive.vue";
-import { ZgetOneCourse } from "@/api/user/index";
-import { Tabs, TabPane } from "element-ui";
+import { ZgetOneCourse, ZgetTeacherInfo } from "@/api/user/index";
+import {
+  Tabs,
+  TabPane,
+  Descriptions,
+  DescriptionsItem,
+  Radio,
+} from "element-ui";
 export default {
   name: "IndexCourse",
   data() {
@@ -75,10 +89,15 @@ export default {
         photo: "",
         intro: "",
         beginTime: "",
+        cord: "",
       },
       teacher: {
-        sex: "",
-        major: "",
+        tname: "",
+        sex: "男",
+        cover: "",
+        place:'',
+        email:"",
+        tcode:'',
       },
     };
   },
@@ -101,6 +120,22 @@ export default {
           this.lesson.photo = result.data.cover;
           this.lesson.intro = result.data.details;
           this.lesson.beginTime = result.data.createTime;
+          this.lesson.cord = result.data.courseCode;
+          let tid = {
+            id: result.data.creatorId,
+          };
+          ZgetTeacherInfo(tid).then((response) => {
+            if (response.msg == "OK") {
+              this.teacher.tname=response.data.name;
+              this.teacher.sex=response.data.sex;
+              this.teacher.cover=response.data.photo;
+              this.teacher.place=response.data.nativePlace;
+              this.teacher.email=response.data.email;
+              this.teacher.tcode=response.data.userName;
+            } else {
+              this.$message.error("获取教师信息失败");
+            }
+          });
         } else {
           this.$message.error("获取课程信息失败");
         }
@@ -110,6 +145,9 @@ export default {
   components: {
     [Tabs.name]: Tabs,
     [TabPane.name]: TabPane,
+    [Descriptions.name]: Descriptions,
+    [DescriptionsItem.name]: DescriptionsItem,
+    [Radio.name]: Radio,
     myLive,
   },
 };
@@ -196,12 +234,12 @@ span {
     padding: 20px 25px 5px;
     overflow: hidden;
     li {
-      width: 180px;
+      // width: 180px;
       margin-right: 10px;
       margin-bottom: 15px;
       font-size: 14px;
       line-height: 20px;
-      float: left;
+      // float: left;
     }
   }
   button {
@@ -209,6 +247,24 @@ span {
     margin: 7px 20px;
     a {
       color: white;
+    }
+  }
+}
+.tes {
+  width: 700px;
+  min-height: 200px;
+  margin: 0 auto;
+  display: flex;
+  .tesmain {
+    width: 50%;
+    div {
+      color: #666;
+      margin-bottom: 20px;
+    }
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
     }
   }
 }
