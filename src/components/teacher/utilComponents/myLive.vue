10992cd23da5b41b6123a61955883d3f6c0e39aa
XY <3400164864@qq.com>
2023-02-07 22:00:20 +0800

    直播

--- src/components/teacher/utilComponents/myLive.vue
+++ src/components/teacher/utilComponents/myLive.vue
@@ -114,27 +114,24 @@ export default {
   },
   methods: {
     liveInit() {
-      if (typeof navigator.getDisplayMedia == "undefined") {
-        this.$confirm(
-          "因为域名原因无法直接开启直播，是否打开网页看操作内容, 是否继续?",
-          "提示",
-          {
-            confirmButtonText: "确定",
-            cancelButtonText: "取消",
-            type: "warning",
-          }
-        )
-          .then(() => {
-            window.open("https://www.cnblogs.com/zqblog1314/p/13233160.html");
-          })
-          .catch(() => {
-            this.$message({
-              type: "info",
-              message: "已取消删除",
-            });
+      this.$confirm(
+        "因为域名原因无法直接开启直播，是否打开网页看操作内容, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          window.open("https://www.cnblogs.com/zqblog1314/p/13233160.html");
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消跳转",
           });
-        return;
-      }
+        });
       this.isShow = true;
       // 获取推流地址
       createPushUrl()
