0a7dab781a2c39143b0e3f3a56f3d90299a7b53a
Administrator <3190493163@qq.com>
2023-02-08 16:12:26 +0800

    yxy修改成绩模块

--- src/pages/student/Achievement.vue
+++ src/pages/student/Achievement.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="mainBox">
     <template>
-      <el-table :data="performanceInfo" style="width: 100%" :header-cell-style="{'text-align': 'center'}">
+      <el-table
+        :data="performanceInfo"
+        style="width: 100%"
+        :header-cell-style="{ 'text-align': 'center' }"
+      >
         <el-table-column prop="term" label="学期" width="180" align="center">
         </el-table-column>
-        <el-table-column prop="timeInterval" label="时间" align="center"> </el-table-column>
-        <el-table-column prop="performance" label="成绩" width="180" align="center">
+        <el-table-column prop="timeInterval" label="时间" align="center">
+        </el-table-column>
+        <el-table-column
+          prop="performance"
+          label="成绩"
+          width="180"
+          align="center"
+        >
         </el-table-column>
       </el-table>
     </template>
@@ -14,7 +24,7 @@
 
 <script>
 import { getPerformance, getTerm } from "@/api/student/yxyAxios";
-import { Table, TableColumn } from "element-ui";
+import { Message, Table, TableColumn } from "element-ui";
 import jwt_decode from "jwt-decode";
 export default {
   data() {
@@ -27,21 +37,33 @@ export default {
     [TableColumn.name]: TableColumn,
   },
   mounted() {
-    getTerm().then((res) => {
-      let info = res.data.slice(-1);
-      let data = {
-        afterTime: info[0].beginTime,
-        beforeTime: info[0].endTime,
-        courseId: this.$route.query.id,
-        userId: jwt_decode(this.$store.state.token).id,
-      };
-      getPerformance(data).then((result) => {
-        result.data.slice(-1)[0].term = info[0].name;
-        result.data.slice(-1)[0].timeInterval =
-          info[0].beginTime + "~" + info[0].endTime;
-        this.performanceInfo.push(result.data.slice(-1)[0]);
+    getTerm()
+      .then((res) => {
+        let info = res.data;
+        let termIds = [];
+        for (let i = 0; i < info.length; i++) {
+          termIds.push({
+            termId: info[i].id,
+            courseId: this.$route.query.id,
+            userId: jwt_decode(this.$store.state.token).id,
+          });
+        }
+        for (let i = 0; i < termIds.length; i++) {
+          getPerformance(termIds[i]).then((result) => {
+            if (result.status == 200) {
+              this.performanceInfo.push({
+                performance: result.data.slice(-1)[0].performance,
+                term: info[i].name,
+                timeInterval: info[i].beginTime + "~" + info[i].endTime,
+              });
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        Message.err("网络异常，数据获取失败！");
       });
-    });
   },
 };
 </script>
